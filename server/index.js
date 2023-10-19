30e44aa0cff9dbcda502529328b3b13c792f1389
baskar <rbaskarkava@gmail.com>
2023-10-19 16:01:12 +0530

    indexjs update

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const connectDatabase = require('./config/db');
 dotenv.config({path:path.join(__dirname,".env")})
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.static(path.join(__dirname,'/client/dist')));
